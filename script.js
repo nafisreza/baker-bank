eb6093bad5011e8ba488c22f1d30b6795e690e0d
nafisreza <68966649+nafisreza@users.noreply.github.com>
2022-12-11 19:51:00 +0000

    Bug fix

--- script.js
+++ script.js
@@ -74,14 +74,17 @@ const labelSumIn = document.querySelector(".summary-value-in");
 const labelSumOut = document.querySelector(".summary-value-out");
 const labelSumInterest = document.querySelector(".summary-value-interest");
 const labelTimer = document.querySelector(".timer");
+
 const containerApp = document.querySelector(".app");
 const containerMovements = document.querySelector(".movements");
+
 const btnLogin = document.querySelector(".login-btn");
 const btnTransfer = document.querySelector(".form-btn-transfer");
 const btnLoan = document.querySelector(".form-btn-loan");
 const btnClose = document.querySelector(".form-btn-close");
 const btnSort = document.querySelector(".btn-sort");
 const btnLogout = document.querySelector(".logout-btn");
+
 const inputLoginUsername = document.querySelector(".login-input-username");
 const inputLoginPassword = document.querySelector(".login-input-password");
 const inputTransferTo = document.querySelector(".form-input-to");
